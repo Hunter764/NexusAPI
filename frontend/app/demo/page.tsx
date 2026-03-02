042b3be3152757652cbacea0dcdf4c487d8d2426
Abhyudaya <kalpanaabhi764@gmail.com>
2026-03-02 23:27:13 +0530

    chore: remove demo sign-in page route.

--- frontend/app/demo/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignInPage } from "@/components/ui/sign-in-flow-1";
-
-export default function DemoOne() {
-  return (
-    <div className="flex w-full min-h-screen justify-center items-center bg-black">
-      <SignInPage />
-    </div>
-  );
-}
